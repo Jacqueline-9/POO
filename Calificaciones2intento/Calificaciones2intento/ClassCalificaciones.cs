362796037e9ac09f487d8a66928e92af72316708
Jacqueline-9 <Jacquelinerosas24@gmail.com>
2020-02-12 20:35:15 -0600

    Calificaciones

---
+++ Calificaciones2intento/Calificaciones2intento/ClassCalificaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificaciones2intento
+{
+    class ClassCalificaciones
+    {
+        //Atributos de la clase 
+        public int calLeida = 0;
+        public int aprobados = 0;
+        public int reprobados = 0;
+        int contador = 0;
+        public double promedio = 0;
+        int suma = 0;
+        
+        //Métodos para retornar promedio 
+        public void capturarCalificación()     
+        {
+            if (calLeida >= 70)
+            {
+                aprobados = aprobados + 1;
+                suma = suma + calLeida;
+            }       
+            else
+            {
+                reprobados = reprobados + 1;
+                suma = suma + calLeida;
+            }
+            contador++;
+            promedio = suma / contador;   
+        }
+    }
+}
