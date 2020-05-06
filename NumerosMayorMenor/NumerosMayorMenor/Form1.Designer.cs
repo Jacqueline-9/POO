beffb28f1bfafba3ba45119bd67ae63393a9fe2c
Jacqueline-9 <Jacquelinerosas24@gmail.com>
2020-05-06 12:11:44 -0500

    Unidad 5

---
+++ NumerosMayorMenor/NumerosMayorMenor/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace NumerosMayorMenor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad de números: ";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCantidad.Location = new System.Drawing.Point(122, 10);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(123, 20);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número: ";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNumero.Enabled = false;
+            this.txtNumero.Location = new System.Drawing.Point(122, 74);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(123, 20);
+            this.txtNumero.TabIndex = 3;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Location = new System.Drawing.Point(170, 100);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(92, 159);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(170, 37);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 6;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Enabled = false;
+            this.btnCalcular.Location = new System.Drawing.Point(170, 130);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 194);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mayor-Menor";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ NumerosMayorMenor/NumerosMayorMenor/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumerosMayorMenor
+{
+    public partial class Form1 : Form
+    {
+        Numeros objnumeros = new Numeros();
+        int cantidadNum = 0;
+        int con = 1;
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (txtCantidad.Text == "")
+            {
+                errorProvider1.SetError(txtCantidad, "Ingresar cantidad");
+                txtCantidad.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtCantidad, "");
+
+            cantidadNum = int.Parse(txtCantidad.Text);
+
+            txtNumero.Enabled = true;
+            btnGuardar.Enabled = true;
+            txtCantidad.Enabled = false;
+            btnEnter.Enabled = false;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtNumero.Text == "")
+            {
+                errorProvider1.SetError(txtNumero, "Ingresar número");
+                txtNumero.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtNumero, "");
+
+            if (con == 1)
+            { objnumeros.arregloNum = new int[cantidadNum]; }
+
+            if (con < cantidadNum)
+            {
+                con++;
+
+                objnumeros.arregloNum[i] = int.Parse(txtNumero.Text);
+
+                txtNumero.Clear();
+
+                i++;
+            }
+            else if (con == cantidadNum)
+            {
+                objnumeros.arregloNum[i] = int.Parse(txtNumero.Text);
+
+                MessageBox.Show("La cantidad de números se registraron correctamente", "Proceso Correcto", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+
+                txtNumero.Clear();
+
+                btnCalcular.Enabled = true;
+            }
+            
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(objnumeros.MayorMenor(), "Número Mayor y Menor", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+
+            txtCantidad.Enabled = true;
+            txtNumero.Enabled = false;
+            btnEnter.Enabled = true;
+            btnGuardar.Enabled = false;
+            btnCalcular.Enabled = false;
+            txtCantidad.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
