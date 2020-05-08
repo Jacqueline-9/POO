c1ed0779698cc8f55338202bef3bbd2adc7efaeb
Jacqueline-9 <Jacquelinerosas24@gmail.com>
2020-05-07 22:36:18 -0500

    Unidad 5

---
+++ FilasColumnasDiagonal/FilasColumnasDiagonal/Arreglos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilasColumnasDiagonal
+{
+    class Arreglos
+    {
+        public int[,] Bidimensional { set; get; }
+        public int[] SumFilas { set; get; }
+        public int[] SumColumnas { set; get; }
+        public int[] Diagonal { set; get; }
+
+
+        public void SumaFilas()
+        {
+            for (int f = 0; f < SumFilas.Length; f++)
+            {
+                int suma = 0;
+                int acum = 0;
+
+                for (int c = 0; c < SumFilas.Length; c++)
+                {
+                    suma = Bidimensional[c, f];
+                    acum += suma;
+                }
+                SumFilas[f] = acum;
+            }
+        }
+
+        public void SumaColumnas()
+        {
+            for (int f = 0; f < SumColumnas.Length; f++)
+            {
+                int suma = 0;
+                int acum = 0;
+
+                for (int c = 0; c < SumColumnas.Length; c++)
+                {
+                    suma = Bidimensional[f, c];
+                    acum += suma;
+                }
+                SumColumnas[f] = acum;
+            }
+        }
+
+        public string SumaDiagonal()
+        {
+            string sumaDiagonal = "";
+            int acum = 0;
+
+            for (int f = 0; f < Diagonal.Length; f++)
+            {
+                int suma = 0;
+
+                for (int c = 0; c < Diagonal.Length; c++)
+                {
+                    if (f == c)
+                    {
+                        suma = Bidimensional[f, c];
+                        acum += suma;
+                    }
+                }
+                Diagonal[f] = acum;
+            }
+            sumaDiagonal = acum.ToString();
+            return sumaDiagonal;
+        }
+
+    }
+}
