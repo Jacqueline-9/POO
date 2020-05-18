53d9f7aae16f1b91a95d87402fc1f869da7cd4bb
Jacqueline-9 <Jacquelinerosas24@gmail.com>
2020-05-18 14:37:17 -0500

    Unidad 5

---
+++ DatosPerritos/DatosPerritos/Perritos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosPerritos
+{
+    class Perritos
+    {
+        public string[,] Perrito { set; get; }
+        public string Nombre { set; get; }
+        public int Edad { set; get; }
+        public double Peso { set; get; }
+        public string Sexo { set; get; }
+        public string[] Color { set; get; }
+        public DateTime FechaIn { set; get; }
+        public DateTime FechaEgre { set; get; }
+        public int[] Promedio { set; get; }
+
+    }
+}
